948d864cd65a49726d215a4f651df89deb3056ac
Eba <50412313+Eba472@users.noreply.github.com>
2020-12-24 23:08:38 +0900

    Better info display

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Button, Popup} from 'semantic-ui-react'
+import { Popover, Button } from 'antd';
+import 'antd/dist/antd.css';
+import {
+    InfoCircleOutlined
+} from '@ant-design/icons';
 
 
 export function Emoji({ country }) {
-    const PopupExample = () => (
-    <Popup
-        trigger={<Button content={country.flag.emoji} />}
-    >
-        <Popup.Header>Country: {country.name}</Popup.Header>
-        <Popup.Content>
-                Capital city is {country.capital}. Land area is {country.area}. Total population is {country.population}. 
-        </Popup.Content>
-        
-    </Popup>
-    )
+    const link = "https://en.wikipedia.org/wiki/"+ country.name
+    const content = (
+        <div>
+            <p>The population is {country.population}.</p>
+            <p>The land area is {country.area}.</p>
+            <h2 className="bigFlag">{country.flag.emoji}</h2>
+            <a color="white" href={link}><InfoCircleOutlined/>  More info</a>
+        </div>
+    );
+    
     return (
-        <PopupExample/>
+        <Popover content={content} title={country.name}>
+            <Button className="flagButton" type="primary" onClick={() => window.location = link}>{country.flag.emoji} </Button>
+        </Popover>
     )
 }
